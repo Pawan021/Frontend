04eee3691524a4b4515a56dc712de71694b9732a
Pawan <Pawank9870@gmail.com>
2023-07-08 06:39:27 +0530

    Navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li className="First">ProShop</li>
+                <li className="Second">Cart</li>
+                <li className="Third">Sign In</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
